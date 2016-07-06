891f5fbb1fb21ea19a9ae0f565d697b288c9a471
deemun <deepak.nayak93@gmail.com>
2016-07-05 20:01:45 -0700

    second commit

---
+++ scraper.js
@@ -0,0 +1,25 @@
+var cheerio = require('cheerio')
+var request = require('request');
+
+request('http://substack.net/images/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+  	let $ = cheerio.load(body);
+  	// console.log(body); //prints out everything
+     // Show the HTML for the page.
+     // body.for
+  //    console.log($('code').html());
+  //   var permission_codes = [];
+  //   $('code').each(function(i, elem) {
+	 //  	permission_codes.push($(this).text());
+		// });
+  //   var url = $('a').attr("href")
+  //   console.log(url)
+  			var permission_codes = [];
+   		$('tr').each(function(i, elem){
+   			permission_
+
+   			});
+   		}
+  }
+})
+
